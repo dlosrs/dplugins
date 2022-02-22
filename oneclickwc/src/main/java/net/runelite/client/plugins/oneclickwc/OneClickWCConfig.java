e3617d13d04a202933ecdce85ab43cf666c014e1
dlosrs <100240741+dlosrs@users.noreply.github.com>
2022-02-22 15:48:47 -0600

    Add files via upload

---
+++ oneclickwc/src/main/java/net/runelite/client/plugins/oneclickwc/OneClickWCConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.oneclickwc;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("oneclickwc")
+public interface OneClickWCConfig extends Config
+{
+}
