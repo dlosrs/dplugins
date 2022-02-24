3835416e9742b7f797d5ba05546eb8a119676131
d24l <dayuliu24@gmail.com>
2022-02-24 16:10:56 -0600

    gem mine

---
+++ oneclickgemmine/src/main/java/net/runelite/client/plugins/oneclickgemmine/OneClickGemMineConfig.java
@@ -0,0 +1,10 @@
+package net.runelite.client.plugins.oneclickgemmine;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("oneclickgemmine")
+public interface OneClickGemMineConfig extends Config
+{
+}
